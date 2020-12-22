8c4b4c75eca73c0796bbfeb2f9a043028378d2da
nonon1234 <funnytoybox94@gmail.com>
2020-12-22 23:33:00 +0900

    goalsテーブルのカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_21_234723) do
+ActiveRecord::Schema.define(version: 2020_12_22_142234) do
+
+  create_table "goals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.date "deadline", null: false
+    t.integer "progress"
+    t.boolean "done", default: false, null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_goals_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "last_name", null: false
@@ -30,4 +42,5 @@ ActiveRecord::Schema.define(version: 2020_12_21_234723) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "goals", "users"
 end
