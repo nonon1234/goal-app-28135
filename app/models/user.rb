1ce6b0e4465d116a5fd51abad8cf5fa345c4ea9a
nonon1234 <funnytoybox94@gmail.com>
2020-12-23 08:34:11 +0900

    バリデーションのエラーメッセージを日本語に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
-    validates :employee_number, numericality: { only_integer: true, message: 'is invalid. Input half-width characters.' }
+    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角で入力してください' }
+    validates :employee_number, numericality: { only_integer: true, message: 'は半角数字で入力してください' }
     validates :division_id, :position_id, numericality: { other_than: 0 }
   end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
-  
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'は英数字混合にしてください'
+
 end
