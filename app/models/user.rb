0c2da36c4be3b2aec12dbef3c0e8ffa43c062693
nonon1234 <funnytoybox94@gmail.com>
2020-12-23 07:43:22 +0900

    userモデルにActiveHashの設定を記述する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :division
+    belongs_to_active_hash :position
+
+    devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
