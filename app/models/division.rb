98c29d17b34b0aa4a7f83f2478ccbc210ceef543
nonon1234 <funnytoybox94@gmail.com>
2020-12-23 07:36:48 +0900

    division,positionにデータを追加

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,2 +1,10 @@
-class Division < ApplicationRecord
+class Division < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '--' },
+    { id: 1, name: '開発部１課' },
+    { id: 2, name: '開発部２課' },
+    { id: 3, name: 'デザイン部１課' },
+    { id: 4, name: 'デザイン部２課' },
+    { id: 5, name: '総務' }
+  ]
 end
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,2 +1,7 @@
-class Position < ApplicationRecord
+class Position < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '--' },
+    { id: 1, name: '一般' },
+    { id: 2, name: '管理' }
+  ]
 end
