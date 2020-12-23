0bca931b0cc6fded9edd01779b51c24d4ed28571
nonon1234 <funnytoybox94@gmail.com>
2020-12-23 23:45:39 +0900

    社員番号が重複して保存できないよう実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ class User < ApplicationRecord
     validates :division_id, :position_id, numericality: { other_than: 0 }
   end
 
+  validates :employee_number, uniqueness: true
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'は英数字混合にしてください'
 end
