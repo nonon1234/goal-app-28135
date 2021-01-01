3e9a9f55398dafcc5f1d8ee9e6ecc0aa20c9a51c
nonon1234 <funnytoybox94@gmail.com>
2021-01-01 19:10:29 +0900

    goalsの全てのアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "goals#index"
-  resources :goals, only: [:index, :new, :create, :show]
+  resources :goals
   
   namespace :admin do
     resources :users, only: [:index]
